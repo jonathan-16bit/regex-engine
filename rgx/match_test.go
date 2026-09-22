5d89990229900cab32f5bd0755a963c529fda50a
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-22 22:04:26 +0530

    feat: add TestAcceptsConcatenatedLiterals() to test accepts()

---
+++ rgx/match_test.go
@@ -0,0 +1,32 @@
+package rgx
+
+import "testing"
+
+func TestAcceptsConcatenatedLiterals(t *testing.T) {
+	aStart, aEnd := literalFragment('a')
+	bStart, bEnd := literalFragment('b')
+
+	start, end := concatenate(aStart, aEnd, bStart, bEnd)
+	end.terminal = true
+
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{input: "ab", want: true},
+		{input: "a", want: false},
+		{input: "b", want: false},
+		{input: "ac", want: false},
+		{input: "abc", want: false},
+		{input: "", want: false},
+	}
+
+	for _, test := range tests {
+		got := accepts(start, test.input)
+
+		if got != test.want {
+			// Errorf instead of Fatal, so that one incorrect case doesnt prevent the others from running
+			t.Errorf("accepts(%q) = %v; want %v", test.input, got, test.want)
+		}
+	}
+}
