b7e6609cba23e1be4ac73a831b2ed80c58c0c3ff
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-22 06:59:29 +0530

    feat: add TestConcatenateFragments() to test concatenate()

--- rgx/state_test.go
+++ rgx/state_test.go
@@ -22,3 +22,42 @@ func TestLiteralFragment(t *testing.T) {
 		t.Fatal("fragment states should have initialized transition maps")
 	}
 }
+
+func TestConcatenateFragments(t *testing.T) {
+	aStart, aEnd := literalFragment('a')
+	bStart, bEnd := literalFragment('b')
+	start, end := concatenate(aStart, aEnd, bStart, bEnd)
+
+	if start != aStart {
+		t.Fatal("incorrect start state returned")
+	}
+
+	if end != bEnd {
+		t.Fatal("incorrect end state returned")
+	}
+
+	aDestinations := aStart.transitions['a']
+	if len(aDestinations) != 1 {
+		t.Fatalf("expected 1 'a' destination, got %d", len(aDestinations))
+	}
+	if aDestinations[0] != aEnd {
+		t.Fatal("transition on 'a' from aStart fails")
+	}
+
+	epsilonDestinations := aEnd.transitions[epsilon]
+	if len(epsilonDestinations) != 1 {
+		t.Fatalf("expected 1 'epsilon' destination, got %d", len(epsilonDestinations))
+	}
+	if epsilonDestinations[0] != bStart {
+		t.Fatal("transition on 'epsilon' from aEnd fails")
+	}
+
+	
+	bDestinations := bStart.transitions['b']
+	if len(bDestinations) != 1 {
+		t.Fatalf("expected 1 'b' destination, got %d", len(bDestinations))
+	}
+	if bDestinations[0] != bEnd {
+		t.Fatal("transition on 'b' from bStart fails")
+	}
+}
