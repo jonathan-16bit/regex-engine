dd8e00b66f1a5bb658942a5235a859626497ed05
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-23 10:50:14 +0530

    feat: add matchConfiguration struct and add visited set logic to accepts(), acceptsFrom()

--- rgx/match.go
+++ rgx/match.go
@@ -1,10 +1,27 @@
 package rgx
 
-func accepts(start *State, input string) bool {
-	return acceptsFrom(start, input, 0)
+type matchConfiguration struct {
+	state *State
+	pos   int
 }
 
-func acceptsFrom(curr *State, input string, pos int) bool {
+func accepts(start *State, input string) bool {
+	visited := make(map[matchConfiguration]bool)
+	return acceptsFrom(start, input, 0, visited)
+}
+
+func acceptsFrom(curr *State, input string, pos int, visited map[matchConfiguration]bool) bool {
+	currConfig := matchConfiguration{
+		state: curr,
+		pos:   pos,
+	}
+
+	if visited[currConfig] {
+		return false
+	}
+
+	visited[currConfig] = true
+
 	// If input exhausted and accepting state reached
 	if pos == len(input) && curr.terminal {
 		return true
@@ -13,7 +30,7 @@ func acceptsFrom(curr *State, input string, pos int) bool {
 	// Epsilon transitions while keeping pos
 	epsilonDestinations := curr.transitions[epsilon]
 	for _, next := range epsilonDestinations {
-		if acceptsFrom(next, input, pos) {
+		if acceptsFrom(next, input, pos, visited) {
 			return true
 		}
 	}
@@ -22,7 +39,7 @@ func acceptsFrom(curr *State, input string, pos int) bool {
 	if pos < len(input) {
 		inputDestinations := curr.transitions[input[pos]]
 		for _, next := range inputDestinations {
-			if acceptsFrom(next, input, pos+1) {
+			if acceptsFrom(next, input, pos+1, visited) {
 				return true
 			}
 		}
