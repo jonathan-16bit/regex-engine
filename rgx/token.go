b6813b0e7edf0c817fa08e3affcfbc0db9ae7237
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-25 04:59:01 +0530

    feat: add tokenKind constants and token struct

---
+++ rgx/token.go
@@ -0,0 +1,19 @@
+package rgx
+
+type tokenKind uint8
+
+const (
+	tokenInvalid tokenKind = iota
+	tokenLiteral
+	tokenStar
+	tokenPipe
+	tokenLeftParen
+	tokenRightParen
+	tokenEOF
+)
+
+type token struct {
+	kind tokenKind
+	ch   uint8
+	pos  int
+}
