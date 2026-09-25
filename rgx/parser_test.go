369806e4fd39545ff253f041c9c2ae93c5abd759
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-25 21:40:12 +0530

    feat: add literal 'a' test to TestParsePrimaryLiteral()

---
+++ rgx/parser_test.go
@@ -0,0 +1,28 @@
+package rgx
+
+import "testing"
+
+func TestParsePrimaryLiteral(t *testing.T) {
+	// First test
+	p := parser {
+		tokens: tokenize("a"),
+	}
+
+	expr, err := p.parsePrimary()
+
+	if err != nil {
+		t.Fatalf("parsePrimary returned an unexpected error: %v", err)
+	}
+
+	if expr == nil {
+		t.Fatal("valid expression must be returned on parsePrimary() for 'a'")
+	}
+
+	if expr.kind != expressionLiteral {
+		t.Fatalf("expected kind expressionLiteral, got %v", expr.kind)
+	}
+
+	if expr.literal != 'a' {
+		t.Fatalf("expected literal 'a', got %v", expr.literal)
+	}
+}
