cb2043eb397948136329a31e70b4e8147364339c
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-25 06:40:46 +0530

    feat: add expressionKind constants and expression struct

---
+++ rgx/ast.go
@@ -0,0 +1,18 @@
+package rgx
+
+type expressionKind uint8
+const (
+	expressionInvalid expressionKind = iota
+	expressionLiteral
+	expressionConcatenation
+	expressionAlternation
+	expressionStar
+)
+
+type expression struct {
+	kind expressionKind
+	literal uint8
+	child *expression
+	left *expression
+	right *expression
+}
